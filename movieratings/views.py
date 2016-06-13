e383a8b168c3b3544dfd14524c0196ad4e61d25c
dhcrain <dhcrain@gmail.com>
2016-06-12 21:26:55 -0400

    All cleaned up, complete

--- movieratings/views.py
+++ movieratings/views.py
@@ -1,22 +1,16 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from movieratings.models import Movie
 from movieratings.models import Rater
 from movieratings.models import Rating
 from movieratings.models import Average
 from movieratings.forms import RatingForm
-from django.db.models import Avg
 import time
 from django.shortcuts import redirect
-
-# Create your views here.
 
 
 def index_view(request):
     context = {
         "top_20": Average.objects.filter(rating_count__gt=100).order_by("-movie_rating")[:20],
-
     }
     return render(request, 'index.html', context)
 
@@ -29,6 +23,7 @@ def movie_view(request, movie_id):
         "rating_count": Average.objects.get(movie_id=movie_id),
         "average_rating": avg_rating.movie_rating/5*100,
         "genre": Movie.genre_getter,
+        "user_rating": Rating.objects.filter(item_id=movie_id).filter(user_id=944)
     }
     return render(request, 'movie.html', context)
 
@@ -51,10 +46,8 @@ def rate_movie(request, movie_id):
             post.item_id = Movie.objects.get(id=movie_id)
             post.rating = form.cleaned_data['rating']
             post.timestamp = time.time()
-            # post = Rating.objects.create(user_id=user_id, item_id=item_id, rating=rating, timestamp=timestamp)
             post.save()
-            return redirect('movie_view', movie_id=movie_id)
-            # return HttpResponseRedirect(reverse('index_view'))
+            return redirect('movieratings.views.movie_view', movie_id=movie_id)
     else:
         form = RatingForm()
 
